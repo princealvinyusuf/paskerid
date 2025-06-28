c5c698485aa35474d08c65c52718f22cca719f9a
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-06-28 17:57:27 +0700

    still same

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,12 +112,12 @@
         <h3>Tren Pencari Kerja</h3>
         <div class="row">
             @foreach($charts as $chart)
-                <div class="col-md-6 mb-4" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $chart->title }}</h5>
-                            <p>{{ $chart->description }}</p>
-                            <canvas id="chart-{{ $chart->id }}" height="200"></canvas>
+                <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
+                    <div class="card shadow-sm rounded-4 border-0" style="min-height: 280px; max-width: 370px; margin: 0 auto;">
+                        <div class="card-body p-3">
+                            <h5 class="card-title mb-2" style="font-size:1.1rem;">{{ $chart->title }}</h5>
+                            <p class="mb-2" style="font-size:0.97rem;">{{ $chart->description }}</p>
+                            <canvas id="chart-{{ $chart->id }}" height="140"></canvas>
                         </div>
                     </div>
                 </div>
