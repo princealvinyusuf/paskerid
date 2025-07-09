faf1270a36152d03ccb10b5cce4b03f30efbb77a
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-07-09 20:52:19 +0700

    Change laravel logo and wording

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Paskerid') }}</title>
+    <title>Pasker ID</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome -->
