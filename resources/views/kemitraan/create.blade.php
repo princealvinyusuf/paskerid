a757d4fd31696f6262d1550613dc6f4f4722e3ff
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-07-11 18:45:16 +0700

    still same

--- resources/views/kemitraan/create.blade.php
+++ resources/views/kemitraan/create.blade.php
@@ -106,7 +106,7 @@
                     <div class="col-md-6">
                         <label for="request_letter" class="form-label">Surat Permohonan Kemitraan</label>
                         <div class="mb-2">
-                            <a href="public/documents/template-surat-permohonan.docx" class="btn btn-outline-secondary btn-sm" download>
+                            <a href="http://www.psid.run.place/paskerid/public/documents/template-surat-permohonan.docx" class="btn btn-outline-secondary btn-sm" download>
                                 <i class="bi bi-download me-1"></i>Download Template Surat Permohonan
                             </a>
                         </div>
