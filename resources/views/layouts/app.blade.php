1ee6ed5a7597f24b093dff79171d56dd9ddb22d2
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-07-11 15:54:43 +0700

    Add navigation bar called kemitraan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,11 @@
                         <a class="nav-link @if(request()->routeIs('news.index')) active fw-bold @endif" href="{{ route('news.index') }}">Berita</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link @if(request()->routeIs('kemitraan.create')) active fw-bold @endif" href="{{ route('kemitraan.create') }}">
+                            <i class="bi bi-handshake me-1"></i> Kemitraan
+                        </a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link @if(request()->routeIs('about')) active fw-bold @endif" href="{{ route('about') }}">Tentang Kami</a>
                     </li>
                 </ul>
