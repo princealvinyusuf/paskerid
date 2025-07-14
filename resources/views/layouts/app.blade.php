a04b7847b1665b07272e2a7fe1d458d42d57ab4f
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-07-14 17:10:11 +0700

    Add text "Pasker ID" besides the logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
         <div class="container">
             <a class="navbar-brand" href="/">
                 <img src="{{ asset('images/logo.png') }}" alt="Paskerid Logo" style="height:40px; width:auto;">
+                Pasker ID
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
