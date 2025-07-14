f0627a14ab20393efd8d9d5bd548b16f7be5c1c6
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-07-14 10:52:00 +0700

    Fixing bug

---
+++ app/Models/HeroStatistic.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HeroStatistic extends Model
+{
+    protected $table = 'hero_statistics';
+} 
--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -8,16 +8,3 @@ class Statistic extends Model
 {
     //
 }
-
-// Duplicated model for hero_statistics
-// File: app/Models/HeroStatistic.php
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class HeroStatistic extends Model
-{
-    protected $table = 'hero_statistics';
-}
