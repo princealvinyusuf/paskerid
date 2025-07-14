57ebbfb067ec0189ae74519c114a7128cc66fd42
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-07-14 10:46:01 +0700

    Added Seeder file of Hero Statistics

---
+++ database/seeders/HeroStatisticsTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class HeroStatisticsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('hero_statistics')->insert([
+            [
+                'title' => 'Jumlah Angkatan Kerja',
+                'value' => '152.107.603',
+                'unit' => '',
+                'description' => 'Total angkatan kerja di Indonesia tahun 2025',
+                'type' => 'hero',
+                'order' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'title' => 'Tingkat Partisipasi Kerja',
+                'value' => '70,63%',
+                'unit' => '%',
+                'description' => 'Persentase partisipasi kerja tahun 2025',
+                'type' => 'hero',
+                'order' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'title' => 'Penduduk Bekerja',
+                'value' => '144.642.004',
+                'unit' => '',
+                'description' => 'Jumlah penduduk bekerja tahun 2025',
+                'type' => 'hero',
+                'order' => 3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+} 
