a18ee30d6c22a488abe81c5a0254650b30cb2992
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-07-15 14:31:43 +0700

    Add Publikasi Section

--- app/Http/Controllers/InformasiPasarKerjaController.php
+++ app/Http/Controllers/InformasiPasarKerjaController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\InformasiSection2;
 use App\Models\InformasiSection3;
 use App\Models\InformasiSection4;
+use App\Models\Publication;
 
 class InformasiPasarKerjaController extends Controller
 {
@@ -16,6 +17,7 @@ class InformasiPasarKerjaController extends Controller
         $informasiSection2 = InformasiSection2::orderBy('order')->get();
         $informasiSection3 = InformasiSection3::orderBy('order')->get();
         $informasiSection4 = InformasiSection4::orderBy('order')->get();
-        return view('informasi_pasar_kerja.index', compact('informasiSection1', 'informasiSection2', 'informasiSection3', 'informasiSection4'));
+        $publikasi = Publication::orderByDesc('date')->take(10)->get();
+        return view('informasi_pasar_kerja.index', compact('informasiSection1', 'informasiSection2', 'informasiSection3', 'informasiSection4', 'publikasi'));
     }
 } 
