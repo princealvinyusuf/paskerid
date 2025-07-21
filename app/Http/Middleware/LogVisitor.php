66ce033b553bfa437d91d21077b8890118bbdf09
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-07-21 18:57:33 +0700

    Add Log Visitor

---
+++ app/Http/Middleware/LogVisitor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Visitor;
+
+class LogVisitor
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!app()->runningInConsole() && !app()->runningUnitTests()) {
+            Visitor::create([
+                'ip_address' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+                'url' => $request->fullUrl(),
+            ]);
+        }
+
+        return $next($request);
+    }
+}
