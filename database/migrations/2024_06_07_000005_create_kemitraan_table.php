3a43c67afab3cb7d3d99716c5b9dd78fb975fc7e
Achmadky04 <achmadky555@gmail.com>
2025-07-23 11:22:51 +0700

    adding pasker facility table migration

--- database/migrations/2024_06_07_000005_create_kemitraan_table.php
+++ database/migrations/2024_06_07_000005_create_kemitraan_table.php
@@ -14,15 +14,18 @@ return new class extends Migration
         Schema::create('kemitraan', function (Blueprint $table) {
             $table->id();
             $table->string('pic_name'); // Nama Penanggung Jawab (PIC)
-            $table->string('pic_position'); // Jabatan Penanggung Jawab
+            // $table->string('pic_position'); // Jabatan Penanggung Jawab
             $table->string('pic_email'); // Alamat Email Penanggung Jawab
             $table->string('pic_whatsapp'); // Nomor WhatsApp Aktif
-            $table->string('sector_category'); // Kategori/Sektor Instansi
+            $table->foreignId('company_sectors_id')->constrained('company_sectors')->onDelete('cascade'); // Kategori/Sektor Instansi
             $table->string('institution_name'); // Nama Instansi
             $table->string('business_sector')->nullable(); // Sektor Lapangan Usaha
             $table->string('institution_address'); // Alamat Instansi
-            $table->string('partnership_type'); // Jenis Kemitraan yang Diajukan
-            $table->text('needs')->nullable(); // Kebutuhan yang Diajukan
+            $table->foreignId('type_of_partnership_id')->constrained('type_of_partnership')->onDelete('cascade'); // Jenis Kemitraan yang Diajukan
+            $table->foreignId('pasker_room_id')->constrained('pasker_room')->nullable(); //Kebutuhan ruangan yang Diajukan
+            $table->string('other_pasker_room')->nullable(); //Kebutuhan ruangan lain yang Diajukan
+            $table->foreignId('pasker_facility_id')->constrained('pasker_facility')->nullable(); // Kebutuhan fasilitas yang Diajukan
+            $table->string('other_pasker_facility')->nullable(); //Kebutuhan fasilitas lain yang Diajukan
             $table->string('schedule')->nullable(); // Usulan Jadwal Kegiatan
             $table->string('request_letter')->nullable(); // Surat Permohonan Kemitraan (file path)
             $table->timestamps();
