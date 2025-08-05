c5651c90dccd7589dc541ddb1828a52a4adb76f1
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-08-05 11:29:49 +0700

    Update Logo pasker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,8 @@
             <!-- Right: Pasker ID Logo and Text -->
             <div class="d-flex align-items-center me-0 pe-2">
                 <img src="{{ asset('images/logo.png') }}" alt="Paskerid Logo" style="height:40px; width:auto;">
-                <span class="fw-bold ms-2 d-none d-lg-inline" style="font-family: inherit; color: inherit;">Pasker ID</span>
+                <img src="{{ asset('images/logo.png') }}" alt="Paskerid Logo" style="height:40px; width:auto;">
+                <!-- <span class="fw-bold ms-2 d-none d-lg-inline" style="font-family: inherit; color: inherit;">Pasker ID</span> -->
             </div>
             <button class="navbar-toggler ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
