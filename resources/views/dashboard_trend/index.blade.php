a0177873917e312ffb480fb554324d4e234513d9
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-10-02 13:23:59 +0700

    Fix container height in dashboard trend

--- resources/views/dashboard_trend/index.blade.php
+++ resources/views/dashboard_trend/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-5">
+<div class="container my-5 dashboard-trend-container">
     <h2>Dashboard Kebutuhan Tenaga Kerja</h2>
     @if($dashboard)
         {!! $dashboard->iframe_code !!}
@@ -9,4 +9,18 @@
         <p>No dashboard trend available.</p>
     @endif
 </div>
-@endsection 
+@push('head')
+<style>
+.dashboard-trend-container {
+    min-height: 90vh;
+}
+.dashboard-trend-container .tableauPlaceholder,
+.dashboard-trend-container .tableauPlaceholder > object,
+.dashboard-trend-container .tableauPlaceholder iframe {
+    width: 100% !important;
+    height: calc(100vh - 180px) !important;
+    overflow: hidden !important;
+}
+</style>
+@endpush
+@endsection
