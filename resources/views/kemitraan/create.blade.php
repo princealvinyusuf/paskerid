05a3fe46077cbbc25739b317da5c9619ef451262
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2026-02-26 11:33:03 +0700

    Fix the stars not showing

--- resources/views/kemitraan/create.blade.php
+++ resources/views/kemitraan/create.blade.php
@@ -1556,6 +1556,7 @@
 
 @push('head')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 @endpush
 
 @push('scripts')
