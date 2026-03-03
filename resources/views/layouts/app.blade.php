d6573fcf15a368e8e98d4468978b55821cc27a71
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2026-03-03 22:29:36 +0700

    UI 1.3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -197,7 +197,7 @@
             </a>
             <!-- Navbar links -->
             <div class="collapse navbar-collapse w-100" id="navbarNav">
-                <ul class="navbar-nav ms-auto gap-4 me-4">
+                <ul class="navbar-nav ms-auto gap-2 me-3">
                     <li class="nav-item">
                         <a class="nav-link @if(request()->routeIs('home')) active fw-bold @endif" href="{{ route('home') }}">Beranda</a>
                     </li>
@@ -434,7 +434,7 @@
     }
     .navbar .nav-link,
     .navbar .dropdown-item {
-        font-size: 0.9rem;
+        font-size: 0.85rem;
     }
     .navbar .nav-link.active, .navbar .nav-link:focus, .navbar .nav-link:hover {
         color: var(--light-green) !important;
