c37636f73a71bec4d7e15b92ae0d263a67f1bc5b
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2026-03-07 11:58:40 +0700

    reduce text in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -781,7 +781,7 @@
     }
     .navbar .nav-link,
     .navbar .dropdown-item {
-        font-size: 0.8rem;
+        font-size: 0.75rem;
     }
     .navbar .nav-link {
         padding-left: 0.45rem;
