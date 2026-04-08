ad2ebd2a0c575992bd0285ebe35ae0f97540fb28
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2026-04-08 10:22:51 +0700

    Change text welcome to notifikasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content welcome-popup-modal">
                     <div class="modal-header border-0 pb-0">
-                        <h5 class="modal-title" id="welcomePopupModalLabel">Welcome</h5>
+                        <h5 class="modal-title" id="welcomePopupModalLabel">Notifikasi</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body pt-2">
