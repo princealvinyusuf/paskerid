4fcd0af9502400c9fefedd01cb4983025e991857
Prince Alvin <princealvinyusuf@gmail.com>
2026-08-04 18:03:19 +0700

    Add certificate

--- app/Services/ProgramKemitraanCertificatePdfService.php
+++ app/Services/ProgramKemitraanCertificatePdfService.php
@@ -39,7 +39,7 @@ class ProgramKemitraanCertificatePdfService
             $streamParts[] = 'Q';
         }
 
-        $streamParts[] = $this->text('F2', 42, $this->centeredX($certificateTitle, 42) + 10.0, 525, strtoupper($certificateTitle));
+        $streamParts[] = $this->text('F2', 42, $this->centeredX($certificateTitle, 42) - 8.0, 525, strtoupper($certificateTitle));
         $streamParts[] = $this->text('F1', 18, $this->centeredX('Dengan ini menyatakan bahwa', 18), 495, 'Dengan ini menyatakan bahwa');
         $streamParts[] = $this->text('F2', 54, $this->centeredX($participantName, 54), 420, strtoupper($participantName));
         $streamParts[] = '0.74 0.12 0.12 RG';
